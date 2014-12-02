253d48f32b549f51348fc7a770d8569a1448f45e
Lindsay <lindsay59000@hotmail.fr>
2014-12-02 11:12:35 +0100

    Mise à jour ClientDAO

--- src/main/java/com/formation/virgin/model/dao/ClientDAO.java
+++ src/main/java/com/formation/virgin/model/dao/ClientDAO.java
@@ -1,7 +1,16 @@
 package com.formation.virgin.model.dao;
 
+import java.util.List;
+
 import com.formation.virgin.model.entity.Client;
 
 public interface ClientDAO extends DAO<Client> {
+
+	/**
+	 * Find all.
+	 *
+	 * @return the list
+	 */
+	List<Client> findAll();
 
 }
