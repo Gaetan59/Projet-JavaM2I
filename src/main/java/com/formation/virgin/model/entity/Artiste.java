f6c949131b42a3e982089439eea1191a6656b153
Gaetan59 <gaetanklein@hotmail.fr>
2014-12-02 12:20:02 +0100

    Pull it

--- src/main/java/com/formation/virgin/model/entity/Artiste.java
+++ src/main/java/com/formation/virgin/model/entity/Artiste.java
@@ -1,5 +1,8 @@
 package com.formation.virgin.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
