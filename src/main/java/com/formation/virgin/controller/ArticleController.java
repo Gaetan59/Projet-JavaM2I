754affe73cef73f10082796fadacd15becaf7948
JMasset <jimato59@gmail.com>
2014-12-02 09:46:20 +0100

    Création du ArticleController

---
+++ src/main/java/com/formation/virgin/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package com.formation.virgin.controller;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.formation.virgin.model.entity.Article;
+import com.formation.virgin.model.manager.MagasinManager;
+
+
+@Controller
+@RequestMapping("/article")
+public class ArticleController {
+		@RequestMapping(method = RequestMethod.GET)
+		public String printClient(final ModelMap model) {
+			final ApplicationContext ctx = new ClassPathXmlApplicationContext(
+					"spring.xml");
+			
+			final MagasinManager magasinManager = (MagasinManager) ctx
+					.getBean("magasinManagerImpl");
+			
+			final List<Article> list = magasinManager.getArticles();
+			
+			model.addAttribute("articles", list);
+			return null;
+		}
+
+}
