08accc20de41c467e5f4a187da5b379c0f407a0a
JMasset <jimato59@gmail.com>
2014-12-02 09:56:16 +0100

    Modif de la classe Article

--- src/main/java/com/formation/virgin/model/entity/Article.java
+++ src/main/java/com/formation/virgin/model/entity/Article.java
@@ -1,42 +1,103 @@
 package com.formation.virgin.model.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * The Class Article.
+ */
 @Entity
 public class Article {
+
+	/** The id. */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer Id;
+	private Integer id;
 
-	private List<Artiste> ListeArtiste = new ArrayList<Artiste>();
+	/** The nom. */
+	private String nom;
 
+	/** The prix. */
+	private Integer prix;
+
+	/**
+	 * Instantiates a new article.
+	 */
 	public Article() {
-
-	}
-
-	public Article(List<Artiste> listeArtiste) {
 		super();
-		ListeArtiste = listeArtiste;
 	}
 
 	/**
-	 * @return the listeArtiste
+	 * Instantiates a new article.
+	 *
+	 * @param nom
+	 *            the nom
+	 * @param prix
+	 *            the prix
 	 */
-	public List<Artiste> getListeArtiste() {
-		return ListeArtiste;
+	public Article(final String nom, final int prix) {
+		this.nom = nom;
+		this.prix = prix;
 	}
 
 	/**
-	 * @param listeArtiste
-	 *            the listeArtiste to set
+	 * Gets the id.
+	 *
+	 * @return the id
 	 */
-	public void setListeArtiste(List<Artiste> listeArtiste) {
-		ListeArtiste = listeArtiste;
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id
+	 *            the new id
+	 */
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the nom.
+	 *
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * Sets the nom.
+	 *
+	 * @param nom
+	 *            the new nom
+	 */
+	public void setNom(final String nom) {
+		this.nom = nom;
+
+	}
+
+	/**
+	 * Gets the prix.
+	 *
+	 * @return the prix
+	 */
+	public Integer getPrix() {
+		return prix;
+	}
+
+	/**
+	 * Sets the prix.
+	 *
+	 * @param prix
+	 *            the new prix
+	 */
+	public void setPrix(final Integer prix) {
+		this.prix = prix;
+
 	}
 }
