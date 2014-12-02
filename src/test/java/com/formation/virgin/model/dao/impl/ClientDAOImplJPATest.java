ce9c9bca8a098be6c0adb516d9caaa50fb5c5aa4
Lindsay <lindsay59000@hotmail.fr>
2014-12-02 16:23:58 +0100

    merge

--- src/test/java/com/formation/virgin/model/dao/impl/ClientDAOImplJPATest.java
+++ src/test/java/com/formation/virgin/model/dao/impl/ClientDAOImplJPATest.java
@@ -2,19 +2,62 @@ package com.formation.virgin.model.dao.impl;
 
 import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.formation.virgin.model.dao.ClientDAO;
+import com.formation.virgin.model.entity.Client;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class ClientDAOImplJPATest {
+	
+	@Mock
+	private final EntityManager em = Mockito.mock(EntityManager.class);
+	
+	@InjectMocks
+	private final ClientDAO clientDao = new ClientDAOImplJPA();
+	
+	
 
 	@Before
 	public void setUp() throws Exception {
+		
+		MockitoAnnotations.initMocks(this);
+		
 	}
 
 	
 	@Test
-	public void TestCreate() {
-		fail("Not yet implemented");
+	public void TestCreate() throws SQLException, ParseException {
+		
+		final Client client = new Client();
+		client.setNom("Dupond");
+		client.setPrenom("Jean");
+		client.setNaissance(new  SimpleDateFormat("dd/mm/yyyy", Locale.FRANCE).parse("20/02/1980"));
+		client.setAdresse("20, rue des pommiers, Lille");
+		client.setEmail("jdupond@gmail.com");
+		
+		final Boolean b1 = clientDao.create(client);
+		Assert.assertTrue(b1);
+		
+		final Boolean b2 = clientDao.create(null);
+		Assert.assertTrue(b2);
+		
 	}
 
 	@Test
