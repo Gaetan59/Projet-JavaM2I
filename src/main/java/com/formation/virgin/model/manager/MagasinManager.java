fe4fb5216cac8467562d36dae2fba24ca23b0ac8
Lindsay <lindsay59000@hotmail.fr>
2014-12-02 10:05:47 +0100

    mise a jour de la Class client et package Manager

--- src/main/java/com/formation/virgin/model/manager/MagasinManager.java
+++ src/main/java/com/formation/virgin/model/manager/MagasinManager.java
@@ -1,10 +1,12 @@
 package com.formation.virgin.model.manager;
 
+import java.util.List;
+
 import com.formation.virgin.model.entity.Article;
 import com.formation.virgin.model.entity.Client;
 import com.formation.virgin.model.entity.Commande;
 
-public class MagasinManager {
+public interface MagasinManager {
 
 	public Boolean addArticle(Article article);
 
