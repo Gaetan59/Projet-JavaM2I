80061c7ff8d9216906ac5a5c7deb2266f26f1074
flo <florian_saphores@hotmail.fr>
2014-12-02 09:09:33 +0100

    un petit commentaire

--- src/main/java/com/formation/virgin/entity/Article.java
+++ src/main/java/com/formation/virgin/entity/Article.java
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-package com.formation.virgin.entity;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Article {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer Id;
-
-	private List<Artiste> ListeArtiste = new ArrayList<Artiste>();
-
-
-	public Article(){
-
-	}
-
-	public Article(final List<Artiste> listeArtiste) {
-		super();
-		ListeArtiste = listeArtiste;
-	}
-
-
-	/**
-	 * @return the listeArtiste
-	 */
-	public List<Artiste> getListeArtiste() {
-		return ListeArtiste;
-	}
-
-
-	/**
-	 * @param listeArtiste
-	 *            the listeArtiste to set
-	 */
-	public void setListeArtiste(final List<Artiste> listeArtiste) {
-		ListeArtiste = listeArtiste;
-	}
-}
-=======
 package com.formation.virgin.entity;
 
 import java.util.ArrayList;
@@ -88,4 +41,3 @@ public class Article {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/Gaetan59/Projet-JavaM2I.git
